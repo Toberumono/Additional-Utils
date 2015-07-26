93c6741fef9871c09a38bb330520e5e2ca326106
Toberumono <JoshuaLipstone@gmail.com>
2015-07-26 00:17:09 -0400

    Minor printout change.

--- src/toberumono/utils/files/NativeLibraryManager.java
+++ src/toberumono/utils/files/NativeLibraryManager.java
@@ -43,7 +43,7 @@ public final class NativeLibraryManager {
 							Files.walkFileTree(unpacked, new RecursiveEraser());
 						}
 						catch (IOException e) {
-							
+							e.printStackTrace();
 						}
 					}
 				});
