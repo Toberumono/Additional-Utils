c38154dcd50bf715651611bce05f9755eb9102d4
Toberumono <JoshuaLipstone@gmail.com>
2015-10-05 15:50:45 -0500

    Reimplemented semifloor for speed and stability.

--- src/toberumono/utils/general/Numbers.java
+++ src/toberumono/utils/general/Numbers.java
@@ -11,8 +11,12 @@ public class Numbers {
 	
 	/**
 	 * While this might not be the best name, it is close to the meaning and a bit of a pun, so, it'll stay for now.<br>
-	 * The semifloor is the largest multiple (closest to positive infinity) of floor(base * fraction) &le; x.<br>
-	 * e.g. {@code semifloor(24, 0.25, 11) = 6} and {@code semifloor(24, 0.5, 11) = 0}
+	 * The semifloor is the largest multiple (closest to positive infinity) of {@code floor(base * fraction)} &le; x.<br>
+	 * e.g. {@code semifloor(24, 0.25, 11) = 6} and {@code semifloor(24, 0.5, 11) = 0}<br>
+	 * Special Cases:
+	 * <ul>
+	 * <li>x equals an infinity --&lt; x</li>
+	 * </ul>
 	 * 
 	 * @param base
 	 *            the base
@@ -22,10 +26,10 @@ public class Numbers {
 	 *            the upper bound
 	 * @return the semifloor of base, fraction, and x
 	 */
-	public static int semifloor(int base, double fraction, double x) {
-		int semifloor = (int) (base * fraction), out = semifloor;
-		for (int step = out + semifloor; step <= x; out = step, step += semifloor);
-		return out;
+	public static double semifloor(int base, double fraction, double x) {
+		if (x == Double.POSITIVE_INFINITY || x == Double.NEGATIVE_INFINITY)
+			return x;
+		return x - x % (base * fraction);
 	}
 	
 }
