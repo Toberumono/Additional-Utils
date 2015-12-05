7ba71c9a3eb465f03cbd95f1bea8e7eb8f132b38
Toberumono <JoshuaLipstone@gmail.com>
2015-12-05 14:12:33 -0800

    Removed Deprecated constructors because they were causing problems for basic lambdas.

--- src/toberumono/utils/files/FileManager.java
+++ src/toberumono/utils/files/FileManager.java
@@ -106,67 +106,6 @@ public class FileManager implements Closeable {
 	}
 	
 	/**
-	 * Constructs a {@link FileManager} on the default {@link FileSystem} with the given actions.
-	 * 
-	 * @param onAdd
-	 *            the function to call on newly added {@link Path Paths} (applies to both files and directories)
-	 * @param onRemove
-	 *            the function to call on newly removed {@link Path Paths} (applies to both files and directories)
-	 * @param onChange
-	 *            the function to call when the {@link WatchService} detects a change in the managed directories
-	 * @throws IOException
-	 *             if an I/O error occurs while initializing the {@link WatchService}
-	 */
-	@Deprecated
-	public FileManager(Consumer<Path> onAdd, Consumer<Path> onRemove, Consumer<WatchKey> onChange) throws IOException {
-		this(onAdd, onRemove, onChange, FileSystems.getDefault());
-	}
-	
-	/**
-	 * Constructs a {@link FileManager} on the given {@link FileSystem} with the given actions.
-	 * 
-	 * @param onAdd
-	 *            the function to call on newly added {@link Path Paths} (applies to both files and directories)
-	 * @param onRemove
-	 *            the function to call on newly removed {@link Path Paths} (applies to both files and directories)
-	 * @param onChange
-	 *            the function to call when the {@link WatchService} detects a change in the managed directories
-	 * @param fs
-	 *            the {@link FileSystem} that the {@link WatchService} will be monitoring
-	 * @throws IOException
-	 *             if an I/O error occurs while initializing the {@link WatchService}
-	 */
-	@Deprecated
-	public FileManager(Consumer<Path> onAdd, Consumer<Path> onRemove, Consumer<WatchKey> onChange, FileSystem fs) throws IOException {
-		this(onAdd, onAdd, onRemove, onRemove, onChange, fs);
-	}
-	
-	/**
-	 * Construct a {@link FileManager} on the given {@link FileSystem} with the given actions.
-	 * 
-	 * @param onAddFile
-	 *            the function to call on newly added files
-	 * @param onAddDirectory
-	 *            the function to call on newly added directories
-	 * @param onRemoveFile
-	 *            the function to call on newly removed files
-	 * @param onRemoveDirectory
-	 *            the function to call on newly removed directories
-	 * @param onChange
-	 *            the function to call when the {@link WatchService} detects a change in the managed directories
-	 * @param fs
-	 *            the {@link FileSystem} that the {@link WatchService} will be monitoring
-	 * @throws IOException
-	 *             if an I/O error occurs while initializing the {@link WatchService}
-	 */
-	@Deprecated
-	public FileManager(Consumer<Path> onAddFile, Consumer<Path> onAddDirectory, Consumer<Path> onRemoveFile, Consumer<Path> onRemoveDirectory,
-			Consumer<WatchKey> onChange, FileSystem fs)
-					throws IOException {
-		this((IOExceptedConsumer<Path>) p -> onAddFile.accept(p), p -> onAddDirectory.accept(p), p -> onRemoveFile.accept(p), p -> onRemoveDirectory.accept(p), k -> onChange.accept(k), fs);
-	}
-	
-	/**
 	 * Recursively adds the {@link Path} and any sub-directories to the {@link FileManager}. This will automatically ensure
 	 * that no events are double-reported. (So you don't need to remove sub-directories before adding a directory that
 	 * contains them)
